a5907c438b073db9b87fc139dabb67a698c42332
Anuraag <anuraag.aravind@gmail.com>
2026-04-16 19:19:50 -0400

    Add files via upload
    Perfboard updates

---
+++ Arduino_fan_logic_v3.ino
@@ -0,0 +1,230 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <SensirionI2CSen5x.h>
+#include <SparkFun_SCD4x_Arduino_Library.h>
+#include "DFRobot_OzoneSensor.h"
+
+//Ozone
+#define COLLECT_NUMBER   20
+#define Ozone_IICAddress OZONE_ADDRESS_3
+/*   iic slave Address, The default is ADDRESS_3
+       ADDRESS_0               0x70      // iic device address
+       ADDRESS_1               0x71
+       ADDRESS_2               0x72
+       ADDRESS_3               0x73
+*/
+DFRobot_OzoneSensor Ozone;
+
+
+SCD4x ozoneSensor;
+SensirionI2CSen5x sen5x;
+
+struct Reading {
+  float value;
+  float upperbound;
+  float lowerbound;
+  bool isactive;
+};
+
+const int number_of_readings = 12;
+Reading readings[number_of_readings];
+int n = 0; //number of iterations
+
+void setup_sen55_sensor(int in1, int in2) {
+  sen5x.begin(Wire);
+  uint16_t error = sen5x.deviceReset();
+  char errorMessage[256];
+    if (error) {
+        Serial.print("Error trying to execute deviceReset(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
+    
+   // Set temperature offset for the sensor
+    float tempOffset = 0.0;
+    error = sen5x.setTemperatureOffsetSimple(tempOffset);
+    if (error) {
+        //Serial.print("Error trying to execute setTemperatureOffsetSimple(): ");
+        errorToString(error, errorMessage, 256);
+        //Serial.println(errorMessage);
+    } else {
+        Serial.print("Temperature Offset set to ");
+        Serial.print(tempOffset);
+        Serial.println(" deg. Celsius (SEN54/SEN55 only)");
+        pinMode(in1, OUTPUT); //OUTPUT means its configured to act as a digital (on / off) output
+        pinMode(in2, OUTPUT);
+    }
+
+  // Start Measurement
+  error = sen5x.startMeasurement();
+}
+
+void setup_ozone_sensor() {
+  ozoneSensor.enableDebugging(); //Gives helpful debugging messages
+  if(ozoneSensor.begin() == false)
+  {
+    Serial.println(F("Sensor not detected. Please check wiring / address. Freezing..."));
+    exit(-1);
+  }
+}
+
+void setup_ozone_two() {
+  Serial.begin(9600); // I believe this is the baud rate. In the ozone only code, this worked perfectly, but here it may need to be adjusted to whatever the current baud rate is.
+  while(!Ozone.begin(Ozone_IICAddress)) {
+    Serial.println("I2c device number error !");
+    delay(1000);
+  }  Serial.println("I2c connect success !");
+/*   Set iic mode, active mode or passive mode
+       MEASURE_MODE_AUTOMATIC            // active  mode
+       MEASURE_MODE_PASSIVE              // passive mode
+*/
+    Ozone.setModes(MEASURE_MODE_PASSIVE);
+}
+
+void setup_fan(int fan_pin) {
+  pinMode(fan_pin, OUTPUT);
+}
+
+void setup_readings() { // TODO
+  readings[0].upperbound = 1000;  // Mass Concentration Pm1p0
+  readings[0].lowerbound = 0;  // Mass Concentration Pm1p0
+  readings[1].upperbound = 1500;  // Mass Concentration Pm2p5
+  readings[1].lowerbound = 5;  // Mass Concentration Pm2p5
+  readings[2].upperbound = 1000000;  // Mass Concentration Pm4p0
+  readings[2].lowerbound = 1000;  // Mass Concentration Pm4p0
+  readings[3].upperbound = 2000;  // Mass Concentration Pm10p0
+  readings[3].lowerbound = 10;  // Mass Concentration Pm10p0
+  readings[4].upperbound = 1000000;  // Ambient Humidity
+  readings[4].lowerbound = 1000;  // Ambient Humidity
+  readings[5].upperbound = 2400;  // Ambient Temperature
+  readings[5].lowerbound = 22;  // Ambient Temperature
+  readings[6].upperbound = 15000;  // VOC Index
+  readings[6].lowerbound = 100;  // VOC Index
+  readings[7].upperbound = 15000;  // nox Index
+  readings[7].lowerbound = 100;  // nox Index
+  readings[8].upperbound = 200;  // Carbon Dioxide
+  readings[8].lowerbound = 100;  // Carbon Dioxide
+  readings[9].upperbound = 3000;  // Temperature
+  readings[9].lowerbound = 22;  // Temperature
+  readings[10].upperbound = 1000000; // Humidity
+  readings[10].lowerbound = 1000; // Humidity
+  readings[11].upperbound = 100; // Ozone
+  readings[11].lowerbound = 5; // Ozone
+}
+
+void setup_bools() {
+  for (int i = 0; i < number_of_readings; ++i) {
+  readings[i].isactive = false;
+  }
+}
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600); 
+  setup_bools();
+  setup_sen55_sensor(2, 3);
+  setup_ozone_sensor();
+  setup_ozone_two();
+ // setup_ozone_2();
+  setup_fan(10);
+  setup_readings();
+}
+
+void read_sen55() {
+ uint16_t error = sen5x.readMeasuredValues(
+    readings[0].value, // Mass Concentration Pm1p0
+    readings[1].value, // Mass Concentration Pm2p5
+    readings[2].value, // Mass Concentration Pm4p0
+    readings[3].value, // Mass Concentration Pm10p0
+    readings[4].value, // Ambient Humidity
+    readings[5].value, // Ambient Temperature
+    readings[6].value, // VOC Index
+    readings[7].value  // nox Index
+  );
+}
+
+void read_ozone() {
+  if (!ozoneSensor.readMeasurement()) { return; }
+  readings[8].value  = ozoneSensor.getCO2();
+  readings[9].value  = ozoneSensor.getTemperature();
+  readings[10].value = ozoneSensor.getHumidity();
+  readings[11].value = Ozone.readOzoneData(COLLECT_NUMBER); //in PPB. 
+  // the prior line may throw an error due to a type mismatch, as value is a float but the reading is in int16_t. In this case, try casting or other debugging methods.
+}
+
+void update_isactives() {
+  bool active = false;
+  float current;
+  for (int i = 0; i < number_of_readings; ++i) {
+    active = readings[i].isactive;
+    current = readings[i].value;
+    if (active){
+      if (current < readings[i].lowerbound){
+        active = false;
+      }
+    }
+    else {
+      if (current > readings[i].upperbound){
+        active = true;
+      }
+    }
+    readings[i].isactive = active;
+  }
+}
+
+//bool should_activate_fan() {
+void fan_control(float FAN) {  
+  bool result = false;
+  for (int i = 0; i < number_of_readings; ++i) {
+  // TODO
+  result = result || readings[i].isactive;
+  }
+  //return result;
+  if (result){
+    digitalWrite(FAN, HIGH);
+  }
+  else {
+    digitalWrite(FAN, LOW);
+  }
+}
+
+void printer() {
+  // particulate matter in units of micrograms per meter cubed
+  Serial.print("Pm1p0:   ");
+  Serial.println(readings[0].value);
+  Serial.print("Pm2p5:   ");
+  Serial.println(readings[1].value);
+  Serial.print("Pm4p0:   ");
+  Serial.println(readings[2].value);
+  Serial.print("Pm10p0:   ");
+  Serial.println(readings[3].value);
+  Serial.print("Humidity from Sen55:   ");
+  Serial.println(readings[4].value);
+  Serial.print("Temp from Sen55:   ");
+  Serial.println(readings[5].value);
+  Serial.print("VOC:   ");
+  Serial.println(readings[6].value);
+  Serial.print("NOX:   ");
+  Serial.println(readings[7].value);
+  Serial.print("CO2:   ");
+  Serial.println(readings[8].value);
+  Serial.print("Temp from Ozone:   ");
+  Serial.println(readings[9].value);
+  Serial.print("Humidity from Ozone:   ");
+  Serial.println(readings[10].value);
+  Serial.print("Ozone");
+  Serial.println(readings[11].value);
+}
+
+void loop() {
+  while (n<100) { //n controls the number of readings.
+  read_sen55();
+  read_ozone();
+  update_isactives();
+  fan_control(12);
+  //quality_descison();
+  printer();
+  n = n+1;
+  }
+  exit;
+}
